b2542fd57c435f4dcfe61e5ecf90f6303542d4f1
LeeYongho91 <skyks4848@naver.com>
2024-02-29 00:26:05 +0900

    clerk add

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full bg-red-500">{children}</div>;
+  return (
+    <div className="flex h-full items-center justify-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
