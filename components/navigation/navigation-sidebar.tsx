c6a8bb2bae9d4810e6cd3a350de58d1eb56a87a1
LeeYongho91 <skyks4848@naver.com>
2024-03-30 18:14:41 +0900

    navi add

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,11 @@ import { redirect } from 'next/navigation';
 import { NavigationAction } from '@/components/navigation/navigation-action';
 import currentProfile from '@/lib/current-profile';
 import { db } from '@/lib/db';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { NavigationItem } from './navigation-item';
+import { ModeToggle } from '../ui/mode-toggle';
+import { UserButton } from '@clerk/nextjs';
 
 const navigationSidebar = async () => {
   const profile = await currentProfile();
@@ -25,6 +30,29 @@ const navigationSidebar = async () => {
   return (
     <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
       <NavigationAction />
+      <Separator className="mx-auto h-[2px] w-10 rounded-md bg-zinc-300 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="mt-auto flex flex-col items-center gap-y-4 pb-3">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: 'h-[48px] w-[48px]',
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
