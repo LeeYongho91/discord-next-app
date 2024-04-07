cdd16fb54371051dae14827f89627640a23b6e67
LeeYongho91 <skyks4848@naver.com>
2024-04-07 23:33:32 +0900

    invite api add

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,5 @@
+const InviteCodePage = () => {
+  return <div>page</div>;
+};
+
+export default InviteCodePage;
--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -1,5 +1,7 @@
 import currentProfile from '@/lib/current-profile';
+import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function PATCH(
   req: Request,
@@ -15,6 +17,18 @@ export async function PATCH(
     if (!params.serverId) {
       return new NextResponse('Server ID Missing', { status: 400 });
     }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+
+    return NextResponse.json(server);
   } catch (error) {
     console.log(error);
   }
