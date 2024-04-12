c90f11f6778109f360e8f4502ae8ee9be03841f8
LeeYongho91 <skyks4848@naver.com>
2024-04-12 23:57:40 +0900

    member modal 추가

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -76,6 +76,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <DropdownMenuItem
               className="cursor-pointer px-3
               py-2 text-sm"
+              onClick={() => onOpen('members', { server })}
             >
               Manage Members
               <Users className="ml-auto h-4 w-4" />
