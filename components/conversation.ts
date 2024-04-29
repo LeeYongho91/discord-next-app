f1b4388b64d23a5304281a524b20a0f459e4836c
LeeYongho91 <skyks4848@naver.com>
2024-04-29 23:56:59 +0900

    feat: conversation setup

--- components/conversation.ts
+++ components/conversation.ts
@@ -1,5 +1,49 @@
-import moduleName from '@/lib/db';
+import { db } from '@/lib/db';
 
-const findConversation = async () => {
-  return await db.conversation.findFirst({});
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  return await db.conversation.findFirst({
+    where: {
+      AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+    },
+    include: {
+      memberOne: {
+        include: {
+          profile: true,
+        },
+      },
+      memberTwo: {
+        include: {
+          profile: true,
+        },
+      },
+    },
+  });
+};
+
+const createNewConversation = async (
+  memberOneId: string,
+  memberTwoId: string,
+) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+  } catch {
+    return null;
+  }
 };
