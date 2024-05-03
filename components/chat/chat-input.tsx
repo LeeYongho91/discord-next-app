dc7d817361f7fb6bb8109bc11f573b90187986c6
LeeYongho91 <skyks4848@naver.com>
2024-05-03 23:58:52 +0900

    test

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,13 +1,55 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
 interface ChatInputProps {
   apiUrl: string;
   query: Record<string, any>;
   name: string;
+  type: 'conversation' | 'channel';
 }
 
-const ChatInput = () => {
-  return <div>ChatInput</div>;
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: '',
+      name: 'test',
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">fda</div>
+              </FormControl>
+            </FormItem>
+          )}
+        ></FormField>
+      </form>
+    </Form>
+  );
 };
 
 export default ChatInput;
