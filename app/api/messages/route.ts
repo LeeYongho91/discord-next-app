d4899689abc7f2a1e8c6fdc31d3771167b053f05
LeeYongho91 <skyks4848@naver.com>
2024-05-23 00:48:05 +0900

    feat messages api route

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,5 +1,9 @@
 import currentProfile from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { Message } from '@prisma/client';
 import { NextResponse } from 'next/server';
+
+const MESSAGES_BATCH = 10;
 
 export async function GET(req: Request) {
   try {
@@ -8,6 +12,39 @@ export async function GET(req: Request) {
 
     const cursor = searchParams.get('cursor');
     const channelId = searchParams.get('channelId');
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!channelId) {
+      return new NextResponse('Channel ID missing', { status: 400 });
+    }
+
+    let messages: Message[] = [];
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    }
   } catch (error) {
     console.log('[MESSAGES_GET]', error);
     return new NextResponse('Internal Error');
